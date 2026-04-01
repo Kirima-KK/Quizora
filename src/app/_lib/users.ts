dc179fe47a98928c389df78ae47378362a60a119
Kirima Kangwanwongsakul <61814979+Kirima-KK@users.noreply.github.com>
2026-04-01 17:10:37 +0700

    Revert "change proxy logic"
    This reverts commit bce827c4bbdc1c57a2895ff75d6976932fbd07e1.

--- src/app/_lib/users.ts
+++ src/app/_lib/users.ts
@@ -1,6 +1,12 @@
+import { cookies } from "next/headers";
 import serverConfig from "../_config/server.config";
 
 export const fetchCurrentUser = async () => {
+  const getCookie = async (name: string) => {
+    return (await cookies()).get(name)?.value ?? '';
+  }
+
+  const cookie = await getCookie('session');
   const res = await fetch(`${serverConfig.backendHost}/api/current-user`, {
     method: 'GET',
     credentials: 'include',
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
-import serverConfig from './app/_config/server.config';
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const token = request.cookies.get('session')?.value;
+
+  console.log("All Cookies:", request.cookies.getAll());
 
   // PUBLIC PATHS
   const isPublicPath = pathname === '/login' || pathname === '/';
@@ -14,21 +16,37 @@ export async function proxy(request: NextRequest) {
     return NextResponse.next();
   }
 
+  if (request.nextUrl.pathname.startsWith("/api") || request.nextUrl.pathname === '/') {
+    return NextResponse.next();
+  }
+
   // VERIFICATION LOGIC
   let payload = null;
-  try {
-    // This fetch MUST have credentials: 'include'
-    const user = await fetch(`${serverConfig.backendHost}/api/current-user`, {
-      method: 'GET',
-      credentials: 'include',
-    });
-
-    if (!user) {
-      NextResponse.redirect(new URL('/login', request.url));
+  if (token) {
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+      const verified = await jwtVerify(token, secret);
+      payload = verified.payload;
+    } catch (err) {
+      console.error("Token invalid:", err);
+      // If token is invalid, clear it and redirect to login
+      if (!isPublicPath) {
+        const response = NextResponse.redirect(new URL('/login', request.url));
+        response.cookies.delete('session');
+        return response;
+      }
     }
-  } catch (error) {
-    console.error("Auth check failed", error);
-    NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  // REDIRECT LOGIC
+  // User is logged in and trying to access /login -> Send to Dashboard
+  if (isPublicPath && payload) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  // User is NOT logged in and trying to access Protected Route -> Send to Login
+  if (!isPublicPath && !payload) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
