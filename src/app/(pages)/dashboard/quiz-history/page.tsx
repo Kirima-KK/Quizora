77347c0f9dc0fcee4c7c135527a0713bcd7fe726
Kirima Kangwanwongsakul <61814979+Kirima-KK@users.noreply.github.com>
2026-04-02 18:39:31 +0700

    remove suspense in page

--- src/app/(pages)/dashboard/quiz-history/page.tsx
+++ src/app/(pages)/dashboard/quiz-history/page.tsx
@@ -5,8 +5,6 @@ import QuizHistory from "@/app/_components/dashboard/quiz-histroy/quiz-history";
 export default function Page() {
 
   return (
-    <Suspense fallback={<QuizesSkeleton />}>
       <QuizHistory />
-    </Suspense>
   );
 }
