74456a5fa0aa81ddaf47fbd2092756a94ead98d2
Kirima Kangwanwongsakul <61814979+Kirima-KK@users.noreply.github.com>
2026-04-02 18:11:53 +0700

    move quiz history to component

---
+++ src/app/_components/dashboard/quiz-histroy/quiz-history.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { fetchUserQuizHistory } from "@/app/_lib/quizes";
+import { fetchCurrentUser } from "@/app/_lib/users";
+import { poppins } from "@/app/_components/ui/font";
+import Quizes from "@/app/_components/quiz/quizes";
+import { Suspense, useEffect, useState } from "react";
+import AuthGuard from "@/app/_components/auth-guard";
+import { QuizCollection } from "@/app/_lib/definition";
+import { QuizesSkeleton } from "@/app/_components/skeleton/quizes-skeleton";
+import { useSearchParams } from "next/navigation";
+
+export default function QuizHistory() {
+  const [quizesHistory, setQuizesHistory] = useState<QuizCollection>();
+  const [loading, setLoading] = useState(true);
+
+  const params = useSearchParams();
+  const currentPage = Number(params.get('page')) || 1;
+
+  useEffect(() => {
+    async function loadQuizHistoryData() {
+      try {
+        setLoading(true);
+
+        // Fetch the user data
+        const user = await fetchCurrentUser();
+
+        // Fetch the user's quizes history
+        const quizesHistory = await fetchUserQuizHistory(user._id, currentPage);
+        setQuizesHistory(quizesHistory);
+      } catch (error) {
+        console.error("Failed to load quiz history data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadQuizHistoryData();
+  }, [currentPage]);
+
+  return (
+    <AuthGuard>
+      <div className={`${poppins.className} flex flex-col gap-4`}>
+        <h1 className="text-[var(--theme-blue)] font-bold text-base md:text-2xl">Quiz History</h1>
+        {loading || !quizesHistory ? <QuizesSkeleton /> : <Quizes quizesData={quizesHistory} noDataText="No quiz history found." />}
+      </div>
+    </AuthGuard>
+  );
+}
