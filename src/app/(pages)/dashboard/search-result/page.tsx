6d64032142d96e78d897fc86b63218ae84aa4d92
Kirima Kangwanwongsakul <61814979+Kirima-KK@users.noreply.github.com>
2026-04-02 18:55:44 +0700

    move use client

--- src/app/(pages)/dashboard/search-result/page.tsx
+++ src/app/(pages)/dashboard/search-result/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = 'force-dynamic';
-
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { fetchQuizByQuery } from "@/app/_lib/quizes";
 import { poppins } from "@/app/_components/ui/font";
