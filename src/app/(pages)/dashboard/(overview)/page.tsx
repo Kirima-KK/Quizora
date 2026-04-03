dfb5e6fcb3440a9aa32935c4dc587cfbae241ff2
Kirima Kangwanwongsakul <61814979+Kirima-KK@users.noreply.github.com>
2026-04-03 12:26:51 +0700

    force dynamic  dashboard

--- src/app/(pages)/dashboard/(overview)/page.tsx
+++ src/app/(pages)/dashboard/(overview)/page.tsx
@@ -1,53 +1,9 @@
-'use client';
+import Dashboard from "@/app/_components/dashboard/dashboard";
 
-import { fetchQuizes } from "@/app/_lib/quizes";
-import Profile from "@/app/_components/dashboard/profile/profile";
-import { poppins } from "@/app/_components/ui/font";
-import Quizes from "@/app/_components/quiz/quizes";
-import { useEffect, useState } from "react";
-import AuthGuard from "@/app/_components/auth-guard";
-import { QuizCollection } from "@/app/_lib/definition";
-import { QuizesSkeleton } from "@/app/_components/skeleton/quizes-skeleton";
-import { useSearchParams } from "next/navigation";
+export const dynamic = 'force-dynamic';
 
 export default function Page() {
-  const [quizes, setQuizes] = useState<QuizCollection>();
-
-  const params = useSearchParams();
-  const currentPage = Number(params.get('page')) || 1;
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function loadDashboardData() {
-      try {
-        setLoading(true);
-
-        // Fetch the quizes data using the current page
-        const quizes = await fetchQuizes(currentPage);
-        setQuizes(quizes);
-      } catch (error) {
-        console.error("Failed to load dashboard data:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    loadDashboardData();
-  }, [currentPage]);
-
   return (
-    <AuthGuard>
-      <div>
-        <main className="flex flex-col gap-12">
-          <Profile />
-
-          <div className="flex flex-col gap-4">
-            <h1 className={`${poppins.className} text-[var(--theme-blue)] font-bold text-base md:text-2xl`}>Featured Quizes</h1>
-            {loading || !quizes ? <QuizesSkeleton /> : <Quizes quizesData={quizes} noDataText="No quiz found." />}
-          </div>
-        </main>
-      </div>
-    </AuthGuard>
+  <Dashboard/>
   );
 }
